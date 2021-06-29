f156f810023c97c35c3bd6ba07d2ab75d772e39e
parix1999 <parisottoedoardo@gmail.com>
2021-06-29 14:35:01 +0200

    js-2.0

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@ new Vue (
     {
         el:'#app',
         data:{
-            // Array da dove parte il dato
+            // Array da dove parte il dato:
             toDoList: [
                 'Fare i compiti',
                 'Fare la spesa',
                 'Fare il bucato',
             ],
-
+            // Dato inserito dall'utente che sarà inserito poi in array: 
+            newTask = '',
         },
     }
 
