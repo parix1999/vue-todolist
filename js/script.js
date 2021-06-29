4b69cde5c95c5ee9db6683ccddc57eaaad7b19de
parix1999 <parisottoedoardo@gmail.com>
2021-06-29 14:31:24 +0200

    js-1.0

---
+++ js/script.js
@@ -0,0 +1,17 @@
+new Vue (
+    {
+        el:'#app',
+        data:{
+            // Array da dove parte il dato
+            toDoList: [
+                'Fare i compiti',
+                'Fare la spesa',
+                'Fare il bucato',
+            ],
+
+        },
+    }
+
+
+
+)
