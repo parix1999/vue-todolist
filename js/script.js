37584f5ebae913d5d73aba024f5caaa995476c29
parix1999 <parisottoedoardo@gmail.com>
2021-06-29 15:06:36 +0200

    verisone-5.0

--- js/script.js
+++ js/script.js
@@ -11,10 +11,15 @@ new Vue (
             // Dato inserito dall'utente che sarà inserito poi in array: 
             newTask : '',
         },
+        mounted(){
+            this.focus();
+        },
         
         methods:{
             // Funzione di aggiunta:
             add:function(){
+                this.focus();
+
                 let push = this.toDoList.push(this.newTask);
                 // Una volta pushato ritorna la stringa vuota:
                 this.newTask = '';
@@ -22,9 +27,16 @@ new Vue (
             },
             // Funzione per togliere:
             remove:function(index){
+                this.focus();
+
                 // Tolgo dall'indice
                 let toRemuve = this.toDoList.splice(index, 1);
                 return toRemuve;
+            },
+            // Funzione per il focus:
+            focus:function(){
+                let prova = document.getElementById('focalize').focus();
+                return prova;
             }
         }
 
